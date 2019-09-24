655735b399ddef95392cb0712977f7eb11fd6f6b
Rich Dutton <rdutton83@gmail.com>
2019-09-24 18:52:26 -0500

    schema added

---
+++ models/Article.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  link: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
